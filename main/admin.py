9812c2599c0ba45121a4b9e2be65fa2e22d8a8f1
Stuart Van Tonder <49988978+YaBoiStui@users.noreply.github.com>
2025-08-26 23:55:25 +0200

    Delete main directory

--- main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import RaceResult, Comment, Event
-
-# Register your models here.
-admin.site.register(RaceResult)
-admin.site.register(Comment)
-admin.site.register(Event)
